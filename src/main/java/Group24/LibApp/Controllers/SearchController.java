d9dcc7fb853346b6c2a23301ba94cf55b23db69e
mangose10 <leonar98@knights.ucf.edu>
2023-02-14 10:41:03 -0500

    Database connection made and tested

--- src/main/java/Group24/LibApp/Controllers/SearchController.java
+++ src/main/java/Group24/LibApp/Controllers/SearchController.java
@@ -1,14 +1,56 @@
 package Group24.LibApp.Controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import Group24.LibApp.Models.User;
+import Group24.LibApp.Repositories.UserRepository;
 
 @RestController
 public class SearchController {
 
-    @GetMapping("/search")
+    @Autowired
+	UserRepository userRepository;
+
+    @GetMapping("/user")
+    public ResponseEntity<List<User>> getAllUsers(@RequestParam(required = false) String username) {
+
+		try {
+			List<User> users = new ArrayList<User>();
+
+			if (username == null)
+                userRepository.findAll().forEach(users::add);
+			else
+                userRepository.findByUsername(username).forEach(users::add);
+
+			if (users.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+
+			return new ResponseEntity<>(users, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+    @GetMapping("/here")
     public String test(){
-        return "test successful";
+        return "here";
     }
 
 }
